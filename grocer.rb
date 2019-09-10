21663ee146d92a7c1e924a7ef6976c66d0befc59
Adam La Rosa <adam.larosa@gmail.com>
2019-09-10 19:22:51 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,36 @@
+require 'pry'
+
 def consolidate_cart(cart)
   # code here
+  
+  final_hash = {}
+  cart.each do |item|
+    name = item.keys[0]
+    props = item.values[0]
+    
+    if final_hash.has_key?(name)
+      final_hash[name][:count] += 1
+    else
+      final_hash[name] = {
+        price: props[:price],
+        clearance: props[:clearance],
+        count: 1
+      }
+    end
+  end
+  final_hash
 end
 
 def apply_coupons(cart, coupons)
   # code here
+  
+  coupons.each do |coupon|
+    item = coupon[:item]
+    
+    if cart[item] &&
+    
+    binding.pry
+  end
 end
 
 def apply_clearance(cart)
