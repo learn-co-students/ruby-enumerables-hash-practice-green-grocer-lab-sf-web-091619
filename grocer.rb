8a50df1f279e1a4dace3c4d952bb3ad3f2ac9090
Arya <arya721@gmail.com>
2019-09-12 02:11:12 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,6 +1,18 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {}
+    cart.each do |items|
+    items.each do |name, attribute|
+      if new_cart[name]
+        new_cart[name][:count] += 1
+      else
+        new_cart[name] = attribute
+        new_cart[name][:count] = 1
+      end
+    end
+  end
+new_cart
 end
+
 
 def apply_coupons(cart, coupons)
   # code here
