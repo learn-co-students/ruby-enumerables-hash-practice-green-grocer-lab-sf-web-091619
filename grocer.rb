accce26693437f647d7463ecbe14c2e6c4f68dec
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-16 04:58:01 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,63 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  result = {}
+  
+  cart.each do |element_hash|
+    element_name = element_hash.keys[0]
+    
+    if result.has_key?(element_name)
+      result[element_name][:count] += 1
+    else
+      result[element_name] = {
+        count: 1,
+        price: element_hash[element_name][:price],
+        clearance: element_hash[element_name][:clearance]
+      }
+    end
+  end
+  
+  result
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    item = coupon[:item]
+    if cart.has_key?(item)
+      if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")         
+        cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num], clearance: cart[item][:clearance], count: coupon[:num]}
+        cart[item][:count] -= coupon[:num]
+      elsif cart[item][:count] >= coupon[:num] && cart.has_key?("#{item} W/COUPON")
+        cart["#{item} W/COUPON"][:count] += coupon[:num]
+        cart[item][:count] -= coupon[:num]
+      end
+    end
+  end
+  cart
 end
+
 
 def apply_clearance(cart)
-  # code here
+  clearance_cart = {}
+  
+  cart.each do |food, info|
+    clearance_cart[food] = {}
+    if info[:clearance] == true
+      clearance_cart[food][:price] = info[:price] * 4 / 5
+    else
+      clearance_cart[food][:price] = info[:price]
+    end
+    clearance_cart[food][:clearance] = info[:clearance]
+    clearance_cart[food][:count] = info[:count]
+  end
+  clearance_cart
+
 end
 
-def checkout(cart, coupons)
-  # code here
+def checkout(array, coupons)
+  hash_cart = consolidate_cart(array)
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  applied_discount = apply_clearance(applied_coupons)
+  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
+  total > 100 ? total * 0.9 : total
 end
